fb89e930d6360e01cb0aada4d118aef511685d26
gantian127 <jamy127@foxmail.com>
2020-02-06 16:09:59 -0700

    add BmiNwmHs in __all__

--- nwm/__init__.py
+++ nwm/__init__.py
@@ -1,7 +1,8 @@
 from .nwm import NwmHs
+from .bmi import BmiNwmHs
 from ._version import get_versions
 
-__all__ = ["NwmHs"]
+__all__ = ["NwmHs", "BmiNwmHs"]
 
 __version__ = get_versions()['version']
 del get_versions
